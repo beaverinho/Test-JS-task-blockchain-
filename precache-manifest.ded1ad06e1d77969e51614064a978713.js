41fa0bac064892a56fac74b989dc45aa6573d38e
beaverinho <48033458+beaverinho@users.noreply.github.com>
2020-09-26 13:54:23 +0300

    Add files via upload

---
+++ precache-manifest.ded1ad06e1d77969e51614064a978713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c3f9fe4f4ab057a9f6e0e462a02a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f4b470e13e2b604259c",
+    "url": "/static/css/main.effa44b0.chunk.css"
+  },
+  {
+    "revision": "b0c6c9ded6c065019d8a",
+    "url": "/static/js/2.57f7cf9f.chunk.js"
+  },
+  {
+    "revision": "6b6e45d96a33f29896bc50f96985ef8a",
+    "url": "/static/js/2.57f7cf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4b470e13e2b604259c",
+    "url": "/static/js/main.559845e8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
